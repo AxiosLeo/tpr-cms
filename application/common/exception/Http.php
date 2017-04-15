bb3d8d8264c367407a692a508c0863e53abf1be1
Axios <axioscros@aliyun.com>
2017-04-15 15:03:59 +0800

    cancel

--- application/common/exception/Http.php
+++ application/common/exception/Http.php
@@ -25,7 +25,6 @@ class Http extends Handle{
             $req['message'] = "something error";
             $req['data'] = [];
             return Response::create($req,'json',"500")->send();
-            die();
         }
     }
 }
