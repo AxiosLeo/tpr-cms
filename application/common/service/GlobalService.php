497136a117190cfb3b7cbddb682c74eda7a113ad
Axios <axioscros@aliyun.com>
2017-04-17 10:44:51 +0800

    GlobalService

---
+++ application/common/service/GlobalService.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/4/17 10:43
+ *
+ * Set:  GlobalService::set($name,$value);
+ *
+ * Get:  GlobalService::get($name);
+ *       $Global = new GlobalService();
+ *       $Global($name);
+ */
+namespace app\common\service;
+class GlobalService{
+    public static function set($name,$value){
+        define($name,$value);
+    }
+
+    public static function get($name=''){
+        if(!defined($name)){
+            return false;
+        }
+        $defined = get_defined_constants(true);
+        if(isset($defined['user'][$name])){
+            return $defined['user'][$name];
+        }
+        return "";
+    }
+
+    public function __invoke($name='')
+    {
+        // TODO: Implement __invoke() method.
+        return self::get($name);
+    }
+}
