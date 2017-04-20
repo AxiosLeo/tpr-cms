89f7ac6e36538343f24c2cd0659c3f1bcb3b1849
Axios <axioscros@aliyun.com>
2017-04-20 16:37:34 +0800

    CryptService

--- config/extra/mongo.php
+++ config/extra/mongo.php
@@ -7,32 +7,34 @@
  * @datetime: 2017/4/10 16:12
  */
 return [
-    "type"              => \think\Env::get('mongo.type'),
-    "hostname"          => \think\Env::get('mongo.hostname'),
-    "database"          => \think\Env::get('mongo.database'),
-    "username"          => \think\Env::get('mongo.username'),
-    "password"          => \think\Env::get('mongo.password'),
-    "hostport"          => \think\Env::get('mongo.hostport'),
-    "dsn"               => \think\Env::get('mongo.dsn'),
-    "params"            => [],
-    "charset"           => "utf8",
-    "pk"                => "_id",
-    "pk_type"           => "ObjectID",
-    "prefix"            => "",
-    "debug"             => false,
-    "deploy"            => 0,
-    "rw_separate"       => false,
-    "master_num"        => 1,
-    "slave_no"          => "",
-    "fields_strict"     => true,
-    "resultset_type"    => "array",
-    "auto_timestamp"    => false,
-    "datetime_format"   => "Y-m-d H:i:s",
-    "sql_explain"       => false,
-    "pk_convert_id"     => false,
-    "type_map" => [
-        "root" =>"array",
-        "document"=>"array",
-        "query" =>"\\think\\mongo\\Query"
+    'default'=>[
+        "type"              => \think\Env::get('mongo.type'),
+        "hostname"          => \think\Env::get('mongo.hostname'),
+        "database"          => \think\Env::get('mongo.database'),
+        "username"          => \think\Env::get('mongo.username'),
+        "password"          => \think\Env::get('mongo.password'),
+        "hostport"          => \think\Env::get('mongo.hostport'),
+        "dsn"               => \think\Env::get('mongo.dsn'),
+        "params"            => [],
+        "charset"           => "utf8",
+        "pk"                => "_id",
+        "pk_type"           => "ObjectID",
+        "prefix"            => "",
+        "debug"             => false,
+        "deploy"            => 0,
+        "rw_separate"       => false,
+        "master_num"        => 1,
+        "slave_no"          => "",
+        "fields_strict"     => true,
+        "resultset_type"    => "array",
+        "auto_timestamp"    => false,
+        "datetime_format"   => "Y-m-d H:i:s",
+        "sql_explain"       => false,
+        "pk_convert_id"     => false,
+        "type_map" => [
+            "root" =>"array",
+            "document"=>"array",
+            "query" =>"\\think\\mongo\\Query"
+        ]
     ]
 ];
