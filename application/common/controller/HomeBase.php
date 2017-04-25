9be3ab9ac0370821a016b12cf8e536cd38fad117
Axios <axioscros@aliyun.com>
2017-04-25 16:12:58 +0800

    admin

--- application/common/controller/HomeBase.php
+++ application/common/controller/HomeBase.php
@@ -38,7 +38,6 @@ class HomeBase extends Controller{
     function __construct(Request $request = null)
     {
         parent::__construct($request);
-        Config::load(APP_PATH.BIND_MODULE."/config.php");
         $this->config = Config::get('setting.admin');
         $this->param = $request->param();
         $this->method = $request->method();
