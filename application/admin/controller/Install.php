63ee21664b6dc3e9ddfc1e465d3e973043d20c74
Axios <axioscros@aliyun.com>
2017-04-25 19:57:14 +0800

    install module

--- application/admin/controller/Install.php
+++ application/admin/controller/Install.php
@@ -17,39 +17,28 @@ class Install extends Controller{
         if(file_exists($lock_file)){
             echo "install.lock exits";die();
         }
-        $env_check = [
-            "system"=>[
-                "title"=>"系统检查",
-                "list"=>[
-                    ["operating system"]
-                ]
-            ]
-        ];
+
         $system = [
             "operating system"=>PHP_OS,
             "php version"=>PHP_VERSION,
             "disk space"=> floor(disk_free_space(APP_PATH) / (1024*1024)).'M'
         ];
         $this->assign('system',$system);
-//        dump($system);
 
         $dir = [
             '/runtime'=>is_writable(APP_PATH."../runtime")?true:false
         ];
         $this->assign('dir',$dir);
-//        dump($dir);
 
         $module = [
             "pdo"=>class_exists('pdo'),
             "redis"=>class_exists('redis'),
         ];
         $this->assign('module',$module);
-//        dump($module);
-//        dump(get_declared_classes());
         return $this->fetch('index');
     }
 
-    private function checkOS(){
-
+    public function database(){
+        return $this->fetch('database');
     }
 }
