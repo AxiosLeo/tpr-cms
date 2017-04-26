5fcd63be6092be405e7dd5a44ca5e5a02016e789
Axios <axioscros@aliyun.com>
2017-04-26 09:31:12 +0800

    update

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -9,9 +9,20 @@
 namespace app\admin\controller;
 
 use app\common\controller\HomeLogin;
+use Phpml\Classification\KNearestNeighbors;
 
 class Index extends HomeLogin {
     public function index(){
-        return $this->fetch('index');
+        $samples = [[1, 3], [2, 1], [2, 4], [3, 1], [4, 1], [4, 2]];
+        $labels = ['a', 'a', 'a', 'b', 'b', 'b'];
+
+        $classifier = new KNearestNeighbors();
+        $classifier->train($samples, $labels);
+
+        $test = $classifier->predict([3, 2]);
+        dump($test);
+        $test = $classifier->predict([3, 4]);
+        dump($test);
+//        return $this->fetch('index');
     }
 }
