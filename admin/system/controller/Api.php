645e883134c5f17e238f6abf89f02e3c36dabc28
Axios <axioscros@aliyun.com>
2017-07-05 17:25:30 +0800

    update

--- admin/system/controller/Api.php
+++ admin/system/controller/Api.php
@@ -10,16 +10,40 @@ namespace admin\system\controller;
 
 use admin\common\controller\HomeLogin;
 use think\Doc;
+use think\Response;
 
 class Api extends HomeLogin{
     public function index(){
-        $api_dir = [
-            ROOT_PATH . 'api/index/controller',
-        ];
-        Doc::config($api_dir, ROOT_PATH . 'api');
-        $doc = Doc::doc();
-        $this->assign('list',$doc);
-        $this->assign('domain',env('api.host',''));
+        if ($this->request->isPost()) {
+            $domain = env('api.host', '');
+            $api_dir = [
+                ROOT_PATH . 'api/index/controller',
+            ];
+            Doc::config($api_dir, ROOT_PATH . 'api');
+            $doc = Doc::doc();
+            $list = [];
+            foreach ($doc as $c) {
+                $class_name = data($c, 'name');
+                if (!empty($c['methods'])) {
+                    foreach ($c['methods'] as $m) {
+                        $api_name = data($m['comment'], 'title');
+                        $api_desc = data($m['comment'], 'desc');
+                        $host = $domain . data($m, 'path');
+                        $method = strtoupper(data($m['comment'], 'method'));
+                        $list[] = [
+                            'api_name' => $api_name,
+                            'api_desc' => $api_desc,
+                            'class_name' => $class_name,
+                            'host' => $host,
+                            'method' => $method
+                        ];
+                    }
+                }
+            }
+            \think\Log::record($this->param, 'debug');
+
+            Response::create($list, 'json')->header([])->send();
+        }
         return $this->fetch('index');
     }
 
--- config/common.php
+++ config/common.php
@@ -36,3 +36,49 @@ if (!function_exists('data')) {
         return isset($array[$index]) ? $array[$index] : $default;
     }
 }
+
+if(!function_exists('array_sort')){
+    function array_sort($array,$sortRule="",$order="asc"){
+        /**
+         * $array = [
+         *              ["book"=>10,"version"=>10],
+         *              ["book"=>19,"version"=>30],
+         *              ["book"=>10,"version"=>30],
+         *              ["book"=>19,"version"=>10],
+         *              ["book"=>10,"version"=>20],
+         *              ["book"=>19,"version"=>20]
+         *      ];
+         */
+        if(is_array($sortRule)){
+            /**
+             * $sortRule = ['book'=>"asc",'version'=>"asc"];
+             */
+            usort($array, function ($a, $b) use ($sortRule) {
+                foreach($sortRule as $sortKey => $order){
+                    if($a[$sortKey] == $b[$sortKey]){continue;}
+                    return (($order == 'desc')?-1:1) * (($a[$sortKey] < $b[$sortKey]) ? -1 : 1);
+                }
+                return 0;
+            });
+        }else if(is_string($sortRule) && !empty($sortRule)){
+            /**
+             * $sortRule = "book";
+             * $order = "asc";
+             */
+            usort($array,function ($a,$b) use ($sortRule,$order){
+                if($a[$sortRule] == $b[$sortRule]){
+                    return 0;
+                }
+                return (($order == 'desc')?-1:1) * (($a[$sortRule] < $b[$sortRule]) ? -1 : 1);
+            });
+        }else{
+            usort($array,function ($a,$b) use ($order){
+                if($a== $b){
+                    return 0;
+                }
+                return (($order == 'desc')?-1:1) * (($a < $b) ? -1 : 1);
+            });
+        }
+        return $array;
+    }
+}
