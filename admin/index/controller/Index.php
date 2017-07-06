a19fd5914081ba73311a516ef67b10c69bc2732c
Axios <axioscros@aliyun.com>
2017-07-06 16:53:32 +0800

    update

--- admin/index/controller/Index.php
+++ admin/index/controller/Index.php
@@ -6,17 +6,35 @@
  * @blog:  http://hanxv.cn
  * @datetime: 2017/5/17 10:10
  */
+
 namespace admin\index\controller;
 
 use admin\common\controller\HomeLogin;
 
-class Index extends HomeLogin {
-    public function index(){
-        $this->assign('menu',$this->menu());
+class Index extends HomeLogin
+{
+    public function index()
+    {
+        $this->assign('menu', $this->menu());
         return $this->fetch('index');
     }
 
-    public function main(){
+    public function main()
+    {
         return $this->fetch('main');
+    }
+
+    public function env()
+    {
+        $data = [
+            'domain' => $this->request->domain(),
+            'os' => PHP_OS,
+            'server_ip' => $_SERVER['SERVER_ADDR'],
+            'server_env' => PHP_VERSION,
+            'api_number' => '',
+            'upload_limit' => ini_get('upload_max_filesize'),
+            'username' => $this->user['username']
+        ];
+        $this->ajaxReturn($data);
     }
 }
