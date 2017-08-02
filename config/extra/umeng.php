a956c041af47fd5df84da71560e6065fabf03e0c
axios <axioscros@aliyun.com>
2017-08-02 10:17:05 +0800

    友盟推送服务配置文件

---
+++ config/extra/umeng.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/8/2 上午10:14
+ */
+
+/**
+ * 友盟推送服务配置文件
+ */
+
+return [
+    'default'=>[
+        'app_key'    => '',
+        'app_secret' => '',
+        'production_mode'=>'false'
+    ]
+];
