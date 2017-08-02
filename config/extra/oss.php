22c323ade93380eacbec9751de923e92eeba8203
axios <axioscros@aliyun.com>
2017-08-02 10:12:05 +0800

    oss配置文件

---
+++ config/extra/oss.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/8/2 上午10:10
+ */
+return [
+    'default'=>[
+        'city'             => '杭州',
+        'network_type'     => '经典网络',
+        'is_internal'      => '',
+        'host'             => 'http://oss-cn-hangzhou.aliyuncs.com/',
+        'access_key_id'    => '',
+        'access_key_secret'=> '',
+    ]
+];
