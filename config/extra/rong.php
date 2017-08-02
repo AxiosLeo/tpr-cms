3bd4be8cde9a5c77ce51eaede7b1ee4c8eaf3c96
axios <axioscros@aliyun.com>
2017-08-02 10:17:20 +0800

    融云IM服务配置文件

---
+++ config/extra/rong.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/8/2 上午10:13
+ */
+
+/**
+ * 融云IM服务配置文件
+ */
+return [
+    'default'=>[
+        'key'    => '',
+        'secret' => '',
+    ]
+];
