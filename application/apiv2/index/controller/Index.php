9400a4aa1657cf6fa0d6f30f24e3a61ba3ff19c8
Axios <axioscros@aliyun.com>
2017-08-23 16:41:12 +0800

    api v2 example

---
+++ application/apiv2/index/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/8/23 16:38
+ */
+namespace tpr\apiv2\index\controller;
+
+use think\Controller;
+
+class Index extends Controller{
+
+    public function index(){
+        $this->response('api-v2');
+    }
+}
---
+++ public/v2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/8/23 16:40
+ */
+
+date_default_timezone_set('Asia/Shanghai');
+
+define('PROJECT_NAME', 'apiv2');
+
+require_once 'init.php';
