e6819285a946b9203293cff6db9f427c3c9ab693
axios <axioscros@aliyun.com>
2017-08-28 15:51:17 +0800

    update

--- config/common.php
+++ config/common.php
@@ -7,20 +7,6 @@
  * @datetime: 2017/5/17 11:30
  */
 
-function is_user_login(){
-    $user = \think\Session::has('user');
-    return  empty($user) ? false:true;
-}
-
-function user_info(){
-    $user = \think\Session::get('user');
-    return $user;
-}
-
-function user_current_id(){
-    $user = user_info();
-    return $user['id'];
-}
 
 function getLastUrl(){
     return \think\Session::get('last_url');
