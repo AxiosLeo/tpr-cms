681b3ece7513a86b332cdf42ec99f0f57dee45b4
axios <axioscros@aliyun.com>
2017-08-29 09:18:29 +0800

    update

--- application/admin/user/controller/Role.php
+++ application/admin/user/controller/Role.php
@@ -17,8 +17,15 @@ class Role extends AdminLogin
     public function index()
     {
         if($this->request->isPost()){
-            $roles = Db::name('role')->select();
-            $count = Db::name('role')->count();
+            $keyword = $this->request->param('keyword','');
+            $roles = Db::name('role')
+                ->where('role_name' , 'like' , '%' . $keyword . '%')
+                ->whereOr('id',$keyword)
+                ->select();
+            $count = Db::name('role')
+                ->where('role_name' , 'like' , '%' . $keyword . '%')
+                ->whereOr('id',$keyword)
+                ->count();
 
             foreach ($roles as &$r) {
                 $r['admin_number'] = Db::name('admin')->where('role_id', $r['id'])->count();
@@ -28,6 +35,19 @@ class Role extends AdminLogin
         }
 
         return $this->fetch('index');
+    }
+
+    public function add(){
+        if($this->request->isPost()){
+            $insert = [
+                'role_name'=>$this->request->param('role_name')
+            ];
+
+            Db::name('role')->insert($insert);
+            $this->success(lang('success'));
+        }
+
+        return $this->fetch();
     }
 
     public function edit(){
@@ -49,6 +69,17 @@ class Role extends AdminLogin
         $this->assign('data' , $role);
 
         return $this->fetch();
+    }
+
+    public function del(){
+        $id = $this->request->param('id');
+
+        $result = Db::name('role')->where('id',$id)->delete();
+        if($result){
+            $this->success(lang('success'));
+        }else{
+            $this->error(lang('error'));
+        }
     }
 
 }
