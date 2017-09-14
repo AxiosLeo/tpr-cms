375100d05d6a3aba55432d0d3d01008438e39ccb
axios <axioscros@aliyun.com>
2017-09-14 09:44:40 +0800

    format

--- library/logic/NodeLogic.php
+++ library/logic/NodeLogic.php
@@ -6,78 +6,82 @@
  * @blog:  http://hanxv.cn
  * @datetime: 2017/8/29 15:49
  */
+
 namespace library\logic;
 
 use think\Db;
 use think\Doc;
 
-class NodeLogic{
+class NodeLogic
+{
 
     public static $roleNode = [];
 
     public static $roleId = 0;
 
-    public static function adminNode($page = 1 , $limit = 10 ,$app_name = APP_NAMESPACE){
-        $class_path = Doc::getClassPathList();
-        $load_path  = [
+    public static function adminNode($page = 1, $limit = 10, $app_name = APP_NAMESPACE)
+    {
+        $load_path = [
             ROOT_PATH . 'library',
             APP_PATH
         ];
 
         $config = [
-            'doc_path'      => $class_path,
-            'load_path'     => $load_path,
+            'doc_path' => Doc::getClassPathList(),
+            'load_path' => $load_path,
             'app_namespace' => $app_name,
         ];
         $doc = Doc::set($config)->doc();
-        $node_list = []; $n = 0;
+        $node_list = [];
+        $n = 0;
 
-        foreach ($doc as $d){
+        foreach ($doc as $d) {
             $methods = $d['methods'];
 
-            foreach ($methods as $m){
-                if(!isset($m['comment']['except'])){
+            foreach ($methods as $m) {
+                if (!isset($m['comment']['except'])) {
                     $node_list[$n++] = [
-                        'title'=>data($m['comment'],'title','未注释'),
-                        'path'=>data($m,'path','')
+                        'title' => data($m['comment'], 'title', '未注释'),
+                        'path'  => data($m, 'path', '')
                     ];
                 }
             }
         }
 
-        $node_list = $page ? array_slice($node_list , ($page-1)*$limit,$limit) : $node_list;
+        $node_list = $page ? array_slice($node_list, ($page - 1) * $limit, $limit) : $node_list;
 
         $menus = Db::name('menu')->field('title , module , controller , func')->select();
         $menu_list = [];
-        foreach ($menus as &$m){
-            $path = $m['module'] .'/' . $m['controller'] . '/' . $m['func'];
+        foreach ($menus as &$m) {
+            $path = $m['module'] . '/' . $m['controller'] . '/' . $m['func'];
             $menu_list[$path] = $m['title'];
         }
 
-        foreach ($node_list as &$nl){
-            if(isset($menu_list[$nl['path']])){
+        foreach ($node_list as &$nl) {
+            if (isset($menu_list[$nl['path']])) {
                 $nl['title'] = $menu_list[$nl['path']];
             }
         }
         return [
             'list' => $node_list,
-            'count'=>$n
+            'count' => $n
         ];
     }
 
-    public static function roleNode($role_id){
-        if(self::$roleId == $role_id && !empty(self::$roleNode)){
+    public static function roleNode($role_id)
+    {
+        if (self::$roleId == $role_id && !empty(self::$roleNode)) {
             return self::$roleNode;
         }
 
         self::$roleId = $role_id;
 
-        $role_node_list = Db::name('role_node')->where('role_id',self::$roleId)->select();
+        $role_node_list = Db::name('role_node')->where('role_id', self::$roleId)->select();
 
         $role_node_array = [];
 
-        foreach ($role_node_list as $l){
-            array_push($role_node_array , $l['node_path']);
+        foreach ($role_node_list as $l) {
+            array_push($role_node_array, $l['node_path']);
         }
 
         self::$roleNode = $role_node_array;
