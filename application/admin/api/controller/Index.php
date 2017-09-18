150610c87a6a18e90086cb05006c89f5d751c2ac
axios <axioscros@aliyun.com>
2017-09-18 14:51:50 +0800

    接口管理功能

--- application/admin/api/controller/Index.php
+++ application/admin/api/controller/Index.php
@@ -8,6 +8,7 @@
  */
 namespace tpr\admin\api\controller;
 
+use library\logic\DocLogic;
 use tpr\admin\common\controller\AdminLogin;
 
 class Index extends AdminLogin{
@@ -16,6 +17,55 @@ class Index extends AdminLogin{
      * @return mixed
      */
     public function index(){
+
+        if($this->request->isPost()){
+            $app_path = $this->request->param('app_path',null);
+            if(empty($app_path)){
+                $app_path = ROOT_PATH . 'application/api';
+            }
+
+            $module_namespace = $this->request->param('module_namespace');
+            $class_name = $this->request->param('class_name',null);
+
+            $api_list = DocLogic::apiList($class_name , $module_namespace , $app_path);
+
+            $total = count($api_list);
+            $this->tableData($api_list,$total);
+        }
+
+        $exception = ['admin'];
+
+        $app_list = DocLogic::appList(ROOT_PATH . 'application/' , $exception);
+        $this->assign('app_list' , $app_list);
+
+        $app_path = ROOT_PATH . 'application/api';
+        $this->assign('app_path',$app_path);
+
         return $this->fetch();
+    }
+
+    public function modules(){
+        if($this->request->isPost()){
+            $app_path = $this->request->param('app_path');
+            $modules = DocLogic::moduleList($app_path);
+            $this->tableData($modules);
+        }
+    }
+
+    public function classes(){
+        if($this->request->isPost()){
+            $app_path = $this->request->param('app_path');
+            $module_namespace = $this->request->param('module_namespace');
+            $class_list = DocLogic::classList($module_namespace, $app_path);
+            $this->tableData($class_list);
+        }
+    }
+
+    public function api(){
+        if($this->request->isPost()){
+            $class_name = $this->request->param('class_name');
+            $api_list = DocLogic::apiList($class_name);
+            $this->tableData($api_list);
+        }
     }
 }
