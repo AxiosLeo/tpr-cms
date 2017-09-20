7d95c3de99c76131a0519ad357af680623d46458
axios <axioscros@aliyun.com>
2017-09-20 09:54:30 +0800

    接口文档

---
+++ application/admin/api/controller/Document.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/9/19 下午1:46
+ */
+
+namespace tpr\admin\api\controller;
+
+use tpr\admin\common\controller\AdminLogin;
+
+class Document extends AdminLogin
+{
+    /**
+     * 接口文档
+     */
+    public function index(){
+
+    }
+}
