16ed86a867213257eeb436b046ff1e35a830f6f8
Axios <axioscros@aliyun.com>
2017-09-22 16:14:21 +0800

    Db::model()测试

--- application/api/index/controller/Index.php
+++ application/api/index/controller/Index.php
@@ -10,6 +10,7 @@
 namespace tpr\api\index\controller;
 
 use library\controller\ApiBase;
+use think\Db;
 use think\Doc;
 
 /**
@@ -58,16 +59,54 @@ class Index extends ApiBase
     public function apiDoc()
     {
         $config = [
-            'doc_path'      => Doc::getClassPathList(),
-            'load_path'     => [ROOT_PATH . 'library',APP_PATH],
+            'doc_path' => Doc::getClassPathList(),
+            'load_path' => [ROOT_PATH . 'library', APP_PATH],
             'app_namespace' => 'tpr\api'
         ];
         $this->response(Doc::set($config)->doc());
     }
 
-    public function name(){
-        $name = $this->request->param('name','name is empty');
+    public function name()
+    {
+        $name = $this->request->param('name', 'name is empty');
 
-        $this->response(['name'=>$name]);
+        $this->response(['name' => $name]);
     }
+
+    public function model()
+    {
+        $start = memory_get_usage();
+        dump('start:' . $start . 'byte');
+
+        Db::model('app');
+        dump(memory_get_usage() . 'byte');
+        Db::model('admin');
+        dump(memory_get_usage() . 'byte');
+        Db::model('menu','mongo.default');
+        dump(memory_get_usage() . 'byte');
+
+        $end = memory_get_usage();
+        dump('end:' . $end . 'byte');
+
+        dump('差值: ' . floor(($end - $start) / 1024) . 'KB');
+
+        echo "<br>------------------------------------<br>";
+
+        $start = memory_get_usage();
+        dump('start:' . $start . 'byte');
+
+        db('app');
+        dump(memory_get_usage() . 'byte');
+        db('admin');
+        dump(memory_get_usage() . 'byte');
+        db('menu');
+        dump(memory_get_usage() . 'byte');
+
+        $end = memory_get_usage();
+        dump('end:' . $end . 'byte');
+
+        dump('差值: ' . floor(($end - $start) / 1024) . 'KB');
+    }
+
+
 }
