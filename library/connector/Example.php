776aff3d8a989cc863e936c772ab7a764aa10aa8
Axios <axioscros@aliyun.com>
2017-10-23 11:24:44 +0800

    数据库连接器

---
+++ library/connector/Example.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author: Axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/10/23 10:59
+ */
+
+namespace library\connector;
+
+/**
+ * Class Example
+ * @package library\connector
+ * @use Example::name($name) ; Example::name($name)
+ */
+class Example
+{
+    protected static $connect = "example";  // config in "CONF_PATH/extra/mysql.php"  and config name is "example"
+    /*
+    protected static $connect = [
+        'type'            => 'mysql',
+        'hostname'        => "",
+        'database'        => "",
+        'username'        => "",
+        'password'        => "",
+        'hostport'        => "",
+        'prefix'          => "",
+        'break_reconnect' => true
+    ];
+    */
+}
