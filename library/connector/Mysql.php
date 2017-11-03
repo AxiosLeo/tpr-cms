bcfe3d905a9919077a9df2171c5e2daf8185f7cf
axios <axioscros@aliyun.com>
2017-11-03 10:25:22 +0800

    Mysql连接器

---
+++ library/connector/Mysql.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axioscros@aliyun.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2017/11/3 10:20
+ */
+
+namespace library\connector;
+
+
+class Mysql
+{
+    protected static $connect = "mysql.default";
+
+    use Base;
+}
