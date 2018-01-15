b03ba00cb45cc9c54dab3b2d809670fe67cb625f
axios <axioscros@aliyun.com>
2018-01-15 14:40:09 +0800

    进制转换器:可设置定长

--- library/logic/ConvertLogic.php
+++ library/logic/ConvertLogic.php
@@ -18,19 +18,20 @@ class ConvertLogic
 {
     public static $index = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static function convert($str , $from, $to, $length = null){
-        if($from!==10){
-            $fromBase =  self::convertTo10($str, $from);
-        }else{
+    public static function convert($str, $from, $to, $length = null)
+    {
+        if ($from !== 10) {
+            $fromBase = self::convertTo10($str, $from);
+        } else {
             $fromBase = $str;
         }
 
         $toBase = self::convert10($fromBase, $to);
 
-        if(!is_null($length)){
-            $strLength  = strlen($toBase);
-            while($strLength < $length){
-                $toBase = "0".$toBase;
+        if (!is_null($length)) {
+            $strLength = strlen($toBase);
+            while ($strLength < $length) {
+                $toBase = "0" . $toBase;
                 $strLength++;
             }
         }
