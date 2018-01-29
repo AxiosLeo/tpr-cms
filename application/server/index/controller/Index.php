f94599aa5992f010b0412e45e30c6ad649443559
axios <axioscros@aliyun.com>
2018-01-29 16:20:32 +0800

    layui update to 2.2.5

--- application/server/index/controller/Index.php
+++ application/server/index/controller/Index.php
@@ -7,13 +7,31 @@
  * @datetime: 2017/10/25 上午10:35
  */
 
-namespace tpr\workman\index\controller;
+namespace tpr\server\index\controller;
 
+use library\extend\workman\lib\Connect;
+use library\extend\workman\lib\Single;
 use think\Controller;
 
 class Index extends Controller
 {
     public function index(){
+//        Single::send($this->request->param('connection_id'),'this is single send');
+//        $this->randSend();
         $this->response($this->request->param());
+    }
+
+    public function randSend(){
+        $client_list = Connect::allConnection();
+        $list = [];
+        foreach ($client_list as $cl){
+            array_push($list,$cl->connection_id);
+        }
+        dump($list);
+
+        $total = count($list);
+        $select = mt_rand(0,$total-1);
+
+        Single::send($list[$select],'this is rand single send:'.$list[$select]);
     }
 }
