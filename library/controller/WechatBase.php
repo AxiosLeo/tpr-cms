d6b9416cb7005c5b13bd12a1a04fccf48ca67e8e
axios <axioscros@aliyun.com>
2018-06-06 11:35:07 +0800

    修改父类

--- library/controller/WechatBase.php
+++ library/controller/WechatBase.php
@@ -9,11 +9,12 @@
 
 namespace library\controller;
 
+use tpr\framework\Controller;
 use tpr\framework\Request;
 use tpr\framework\Session;
 use Wechat\WechatOauth;
 
-class WechatBase extends HomeBase
+class WechatBase extends Controller
 {
     protected $Oauth;
 
