c3e1fb6d10126b1ef7a7d17f2cbfafe301da406e
axios <axioscros@aliyun.com>
2018-07-19 15:19:57 +0800

    无限层级生成树；遍历生成树，生成节点列表

--- config/common.php
+++ config/common.php
@@ -67,5 +67,58 @@ if(!function_exists('getDayBeginEndTime')){
             ];
         }
     }
+}
 
+if (!function_exists('tpr_infinite_tree')) {
+    /**
+     * 无限层级的生成树方法
+     * @param $data
+     * @param string $parent_index
+     * @param string $data_index
+     * @param string $child_name
+     * @return array|bool
+     */
+    function tpr_infinite_tree($data, $parent_index = 'parent_id', $data_index = 'id', $child_name = 'child')
+    {
+        $items = [];
+        foreach ($data as $d) {
+            $items[$d[$data_index]] = $d;
+            if (!isset($d[$parent_index]) || !isset($d[$data_index]) || isset($d[$child_name])) {
+                return false;
+            }
+        }
+        $tree = [];
+        $n = 0;
+        foreach ($items as $item) {
+            if (isset($items[$item[$parent_index]])) {
+                $items[$item[$parent_index]][$child_name][] = &$items[$item[$data_index]];
+            } else {
+                $tree[$n++] = &$items[$item[$data_index]];
+            }
+        }
+        return $tree;
+    }
+}
+
+if (!function_exists('traversal_tree_to_node_list')) {
+    /**
+     * 遍历生成树，生成节点列表
+     * @param $tree
+     * @param array $data
+     * @param int $layer
+     * @param string $layer_name
+     * @param string $child_name
+     */
+    function traversal_tree_to_node_list($tree, &$data = [], $layer = 0, $layer_name = 'layer', $child_name = 'child')
+    {
+        foreach ($tree as $t) {
+            $node = $t;
+            unset($node[$child_name]);
+            $node[$layer_name] = $layer;
+            $data[] = $node;
+            if (isset($t[$child_name]) && !empty($t[$child_name])) {
+                traversal_tree_to_node_list($t[$child_name], $data, $layer + 1);
+            }
+        }
+    }
 }
