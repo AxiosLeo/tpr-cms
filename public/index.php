9a958e64de4ece78679e12f492d4445c59e55ffd
axios <axioscros@aliyun.com>
2018-07-25 14:07:34 +0800

    入口支持应用切换

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 
 date_default_timezone_set('Asia/Shanghai');
 
-define('PUBLIC_PATH',__DIR__);
-define('PROJECT_NAME', 'admin');
+define('PUBLIC_PATH', __DIR__);
+
+$project_name = isset($_GET['app']) && in_array($_GET['app'], ['admin','api','apiv2','wechat','test']) ? $_GET['app'] : 'admin';
+
+define('PROJECT_NAME', $project_name);
 
 require_once '../init.php';
 
