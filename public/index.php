0052dac92c292bce432041ecac7bc63d08b35d9c
axios <axioscros@aliyun.com>
2018-07-25 14:30:42 +0800

    nginx config

--- public/index.php
+++ public/index.php
@@ -3,10 +3,19 @@
 date_default_timezone_set('Asia/Shanghai');
 
 define('PUBLIC_PATH', __DIR__);
+define("APP_KEY",'y8kA7atkk7TrCgS8orObNnXMl54QlBnS');
 
-$project_name = isset($_GET['app']) && in_array($_GET['app'], ['admin', 'api', 'apiv2', 'wechat', 'test']) ? $_GET['app'] : 'admin';
-
+$project_name = isset($_GET[APP_KEY]) && in_array($_GET[APP_KEY], ['admin', 'api', 'apiv2', 'wechat', 'test']) ? $_GET[APP_KEY] : 'admin';
+unset($_GET[APP_KEY]);
 define('PROJECT_NAME', $project_name);
+
+/**
+    #nginx config
+    location /api/ {
+        rewrite ^(.*)$ /index.php?y8kA7atkk7TrCgS8orObNnXMl54QlBnS=api last;
+        break;
+    }
+*/
 
 require_once '../init.php';
 
