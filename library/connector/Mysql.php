fcb14864fc8799d987ff1a14e54d58eec543606e
axios <axioscros@aliyun.com>
2018-11-30 16:40:08 +0800

    connector/ 使用Db类库中的DbClient构建自定义数据库连接器

--- library/connector/Mysql.php
+++ library/connector/Mysql.php
@@ -8,16 +8,21 @@
 
 namespace library\connector;
 
+use tpr\db\DbClient;
+use tpr\db\DbFacade;
+use tpr\framework\Config;
+
 /**
  * Class Example
  * @package library\connector
  * @example Example::name($name) ; Example::name($name)
  */
-class Mysql extends MysqlFacade
+class Mysql extends DbFacade
 {
     public static function __callStatic($method, $params)
     {
-        $Con = Connector::newCon('mysql.default', 'mysql.default');
+        $config = Config::get('mysql.default');
+        $Con = DbClient::newCon('mysql.default', $config);
         return call_user_func_array([$Con, $method], $params);
     }
 }
