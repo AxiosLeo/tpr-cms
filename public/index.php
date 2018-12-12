19e7fc762bfede6165d9abb9c51f977b2abf0bdc
axios <axioscros@aliyun.com>
2018-12-12 14:41:14 +0800

    import library

--- public/index.php
+++ public/index.php
@@ -13,9 +13,10 @@ define('APP_PATH', ROOT_PATH . 'application' . DS . PROJECT_NAME . DS);
 define('RUNTIME_PATH', ROOT_PATH . 'runtime' . DS . PROJECT_NAME . DS);
 define('ROUTE_PATH', ROOT_PATH . 'route' . DS . PROJECT_NAME . DS);
 
-
 // 加载基础文件
 require __DIR__ . '/../framework/base.php';
+
+Loader::addNamespace('library', ROOT_PATH . 'library' . DS);
 
 // 支持事先使用静态方法设置Request对象和Config对象
 // 执行应用并响应
