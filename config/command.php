75d5986edd555ddd614af53527947d3056de1354
axios <axioscros@aliyun.com>
2018-12-30 23:53:58 +0800

    gearman 命令行工具

--- config/command.php
+++ config/command.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * @author: axios
- * @email: axiosleo@foxmail.com
- * @blog:  http://hanxv.cn
+ * @author  : axios
+ * @email   : axiosleo@foxmail.com
+ * @blog    :  http://hanxv.cn
  * @datetime: 2018/5/15 20:09
  */
 
 return [
     'tpr\command\Index',
-    'tpr\command\Test'
+    'tpr\command\Gearman',
+    'tpr\command\Test',
 ];
