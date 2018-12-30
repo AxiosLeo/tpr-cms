d98e19a34702287952463c10ea16bbd253050b76
axios <axioscros@aliyun.com>
2018-12-31 01:05:39 +0800

    自动获取command目录下的自定义命令行

--- config/command.php
+++ config/command.php
@@ -6,8 +6,25 @@
  * @datetime: 2018/5/15 20:09
  */
 
-return [
-    'tpr\command\Index',
-    'tpr\command\Gearman',
-    'tpr\command\Test',
-];
+$dir  = APP_PATH;
+$list = [];
+if (is_dir($dir)) {
+    $dir = substr($dir, -1) != DIRECTORY_SEPARATOR ? $dir . DIRECTORY_SEPARATOR : $dir;
+    if (!file_exists($dir)) {
+        if (!mkdir($dir, 0700, true)) {
+            return null;
+        }
+    }
+
+    $dirHandle = opendir($dir);
+    while (false !== ($fileName = readdir($dirHandle))) {
+        $subFile = $dir . $fileName;
+        $tmp     = str_replace('.', '', $fileName);
+        if (!is_dir($subFile) && $tmp != '' && !in_array($fileName, ["Base.php"])) {
+            array_push($list, APP_NAMESPACE . "\\" . basename($subFile, ".php"));
+        }
+    }
+    closedir($dirHandle);
+}
+
+return $list;
