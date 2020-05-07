726c241dd8ac8ec8279b94f3fa02dc89339bb01f
axios <axiosleo@foxmail.com>
2020-05-08 06:16:45 +0800

    main page

--- application/admin/func.php
+++ application/admin/func.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 use tpr\Container;
 
+if (!function_exists('getDayBeginEndTime')) {
+    function getDayBeginEndTime($date, $format='timestamp')
+    {
+        $begin = strtotime($date . ' 00:00:00');
+        $end   = strtotime("{$date} +1 day -1 seconds");
+        if ('timestamp' == $format) {
+            return [
+                'begin'=> $begin,
+                'end'  => $end,
+            ];
+        }
+
+        return [
+            'begin'=> date($format, $begin),
+            'end'  => date($format, $end),
+        ];
+    }
+}
+
 return [
-    'testFunc' => function ($a) {
-        return $a . '-test';
-    },
     'selected' => function ($menuModule, $currModule) {
         return $menuModule === $currModule;
     },
@@ -15,9 +31,9 @@ return [
         call_user_func_array('dump', $data);
     },
     'url'      => function ($module, $controller, $action) {
-        // @var \tpr\core\request\RequestAbstract $request
+        // @var \tpr\core\request\DefaultRequest $request
         $request = Container::get('request');
 
-        return $request->url() . '/' . $module . '/' . $controller . '/' . $action;
+        return $request->indexFile() . '/' . $module . '/' . $controller . '/' . $action;
     },
 ];
--- application/admin/index/controller/Index.php
+++ application/admin/index/controller/Index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace admin\index\controller;
 
 use admin\common\controller\AdminLogin;
+use tpr\Path;
 
 class Index extends AdminLogin
 {
@@ -15,6 +16,64 @@ class Index extends AdminLogin
 
     public function main()
     {
+        if ($this->request->isPost()) {
+            $disk_size = floor(disk_free_space(Path::root()) / (1024 * 1024));
+            $disk_size = $disk_size < 100 ? '磁盘空间已小于100M' : $disk_size . 'M';
+            $data      = [
+                'domain'       => $this->request->host(),
+                'os'           => PHP_OS,
+                'server_ip'    => $this->request->server('SERVER_ADDR'),
+                'server_env'   => PHP_VERSION,
+                'disk'         => $disk_size,
+                'username'     => 'AxiosCros',
+                'upload_limit' => ini_get('upload_max_filesize'),
+            ];
+            $req = [
+                'env'                => $data,
+                'users_number'       => rand(10000, 99999),
+                'users_number_today' => rand(0, 100),
+            ];
+            $this->response($req);
+        }
+        $this->assign('user_number', rand(10000, 99999));
+        $this->assign('register_today', rand(0, 100));
+
         return $this->fetch();
+    }
+
+    public function user()
+    {
+        $dayArr   = $this->getLastSevenDay();
+        $day_list = [];
+        $data     = [];
+        foreach ($dayArr as $d) {
+            $day_list[] = $d['day'];
+            $data[]     = rand(0, 100);
+        }
+        $this->success([
+            'day'  => $day_list,
+            'data' => $data,
+        ]);
+    }
+
+    private function getLastSevenDay()
+    {
+        $time = time();
+
+        $dayArr = [];
+        $n      = 0;
+
+        $total = 7;
+
+        for ($i = 0; $i < $total; ++$i) {
+            $timestamp           = $time - ($total - $n) * 3600 * 24;
+            $dayArr[$n]['day']   = date('Y-m-d', $timestamp);
+            $temp                = getDayBeginEndTime($dayArr[$n]['day']);
+            $dayArr[$n]['begin'] = $temp['begin'];
+            $dayArr[$n]['end']   = $temp['end'];
+            ++$n;
+        }
+
+        return $dayArr;
     }
 }
