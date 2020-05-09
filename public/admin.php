b715d78e9d39173443b2d8e1e09662672526ac5f
axios <axiosleo@foxmail.com>
2020-05-10 07:28:36 +0800

    update

--- public/admin.php
+++ public/admin.php
@@ -1,6 +1,5 @@
 <?php
 
 define('APP_NAME', 'admin');
-define('DEBUG', false);
 
 require_once __DIR__ . \DIRECTORY_SEPARATOR . 'base.php';
--- public/func.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'testFunc' => function ($a) {
-        return $a . '-test';
-    },
-];
