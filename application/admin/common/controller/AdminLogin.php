cba9c871a249b0b63952316c6dac2326974deeee
axios <axiosleo@foxmail.com>
2020-05-10 19:38:38 +0800

    check login status, assign user info

--- application/admin/common/controller/AdminLogin.php
+++ application/admin/common/controller/AdminLogin.php
@@ -5,16 +5,28 @@ declare(strict_types=1);
 namespace admin\common\controller;
 
 use admin\common\models\Menu;
+use admin\common\User;
+use function cms\createUrl;
 
 class AdminLogin extends AdminBase
 {
+    /**
+     * @var User
+     */
+    protected $user;
+
     public function __construct()
     {
         parent::__construct();
+        $this->user = new User();
+        if (!$this->user->isLogin()) {
+            $this->redirect(createUrl('user', 'index', 'login'));
+        }
         $MenuModel = new Menu();
         if (empty($MenuModel->findAll())) {
             $MenuModel->init();
         }
         $this->assign('menu', $MenuModel->getMenu());
+        $this->assign('user_info', $this->user->getInfo());
     }
 }
