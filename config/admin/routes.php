8b4299974888c4ef0f9d8330f531a417c1c1c6a4
axios <axiosleo@foxmail.com>
2020-05-10 19:41:40 +0800

    add login route

--- config/admin/routes.php
+++ config/admin/routes.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 // @see : https://github.com/nikic/FastRoute
 return [
-    'home' => [
+    'home'  => [
         'rule'         => '/test[/{title}]',
         'method'       => 'GET',
         'handler'      => 'admin\\index\\controller\\Index::index',
         'requirements' => [],
+    ],
+    'login' => [
+        'rule'    => '/login',
+        'method'  => 'GET',
+        'handler' => 'admin\\user\\controller\\Index::login',
     ],
 ];
