52ceb37cb7bd161c2b5990f9188e65208fdedd3f
axios <axiosleo@foxmail.com>
2020-05-10 19:40:31 +0800

    add createUrl method

--- library/helper.php
+++ library/helper.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+namespace cms;
+
+use tpr\Container;
+
 function data($array, $index, $default = '')
 {
     return isset($array[$index]) ? $array[$index] : $default;
@@ -9,6 +13,14 @@ function data($array, $index, $default = '')
 
 function halt(...$data)
 {
-    call_user_func_array('dump', $data);
+    \call_user_func_array('dump', $data);
     die();
+}
+
+function createUrl($module, $controller, $action)
+{
+    // @var \tpr\core\request\DefaultRequest $request
+    $request = Container::get('request');
+
+    return $request->indexFile() . '/' . $module . '/' . $controller . '/' . $action;
 }
