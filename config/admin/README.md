f958483c3d68f0948ff202b261a607ffc2f05c0f
axios <axiosleo@foxmail.com>
2020-11-22 11:36:09 +0800

    chore: add admin configuration dir

---
+++ config/admin/README.md
@@ -0,0 +1,1 @@
+you can write configuration files in here.
--- config/cli/cli.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'name'      => 'Command Tools for TPR application',
-    'version'   => '1.0.0',
-    'namespace' => 'app\\command',
-];
