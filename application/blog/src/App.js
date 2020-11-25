6b97323e380cb1eac78ad5762e9ce943489b9640
axios <axiosleo@foxmail.com>
2020-11-25 22:07:26 +0800

    add .eslintrc

--- application/blog/src/App.js
+++ application/blog/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> developing...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hanxv.cn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Blog Link
         </a>
       </header>
     </div>
