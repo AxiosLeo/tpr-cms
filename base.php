996c7bbbebe767c58f4af355e2348120c25a641a
AxiosCros <axioscros@aliyun.com>
2021-05-29 23:43:11 +0800

    perf: cgi index

--- base.php
+++ base.php
@@ -6,7 +6,11 @@ use tpr\App;
 use tpr\Config;
 use tpr\Path;
 
-$autoload_file = __DIR__ . '/../vendor/autoload.php';
+$autoload_file = __DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+
+if (!\defined('APP_NAME')) {
+    exit(0);
+}
 
 if (!file_exists($autoload_file)) {
     echo 'Please install composer libraries with command : `composer install`';
@@ -14,7 +18,7 @@ if (!file_exists($autoload_file)) {
     exit();
 }
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
 
 // use debug mode
 App::debugMode(true);
@@ -31,7 +35,3 @@ $app = App::default();
 
 // load global config
 Config::load(Path::join(Path::root(), 'config/global'));
-$app->config([
-    'name'      => APP_NAME,
-    'namespace' => APP_NAME,
-])->run();
--- public/index.php
+++ public/index.php
@@ -2,4 +2,9 @@
 
 define('APP_NAME', 'admin');
 
-require_once __DIR__ . \DIRECTORY_SEPARATOR . 'base.php';
+require_once __DIR__ . \DIRECTORY_SEPARATOR . '..' . \DIRECTORY_SEPARATOR . 'base.php';
+
+$app->config([
+    'name'      => APP_NAME,
+    'namespace' => APP_NAME,
+])->run();
